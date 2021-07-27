3c1e56a64cbf676c61b6c4595febcadb10d4d68d
Javier <74420593+JavierWhite@users.noreply.github.com>
2021-07-27 16:21:25 -0600

    Update countlinesPractice.pl

--- countlinesPractice.pl
+++ countlinesPractice.pl
@@ -12,3 +12,28 @@ close(FH);                      # closing file
 
 my $count = scalar @lines;      # The number of lines in the file
 say "There are $count lines in $filename";
+
+#####################################################################################################################################################################
+
+##                                                                        Method 2
+
+#!/usr/bin/perl
+# count2.pl
+
+use 5.18.0;
+use warnings;
+use IO::File;
+
+my $fh = IO::File->new( $filename, "r");
+if(! $fh){
+    print("Cannot open $filename ($!)\n")
+    exit;
+}
+
+my $count = 0;
+while ($fh->getline){
+    $count++;
+}
+
+$fh->close;
+print("There are $count lines in $filename\n");
